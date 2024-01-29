2547fe26eac5998f3a5a944c7edc00dd0b8e0966
Abu-Huraira7 <hurairaprince889@gmail.com>
2024-01-29 23:53:18 +0500

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+
+//Best conversions--------
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1)  // we face problem is such conditions when the DATATYPE of comparisons value are not same
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log( null == 0)
+console.log( null >= 0)
+
+console.log(undefined > 0)
+console.log(undefined == 0 )
+console.log(undefined >= 0) // in most of the cases we avoid these types of comparisons
+
+//we always try our best to gave priority to clean code
+
+//=== strict check check values and also check its datatype
+
+console.log("2" ===1 )
