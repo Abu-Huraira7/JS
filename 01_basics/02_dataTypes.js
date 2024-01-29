1ed70b8489528f5e6fd6a1fda6f491bc4433a72a
Abu-Huraira7 <hurairaprince889@gmail.com>
2024-01-29 21:40:18 +0500

    "updated data"

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3+3)
+                        // code readability that should be high
+console.log("Huraira")
+
+
+let name = "Abu Huraira"
+let age = 18
+let isLoggedIn = false
+
+console.table([name, age, isLoggedIn])
+
+
+        // Primitive DataType
+// number => 2 to power 53
+// bigInt => for large range of numbers
+// string => ""
+// boolean => true/false
+// null => standAlone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
